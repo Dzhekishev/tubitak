c03ee0f8414a2f53cdf2be02a33ef981dab953d8
Dzhekishev <djekishev03@gmail.com>
2025-11-26 17:13:47 +0600

    fix setting path

--- tubitak/tubitak/settings.py
+++ tubitak/tubitak/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tubitak.tubitak.myapp',
+    'myapp',
     'rest_framework',
     'django_filters',
     'django.contrib.postgres',
